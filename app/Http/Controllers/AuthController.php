69b94a496a0215b1e7057f7235a9ad61fbb244ac
Azizbek Isroilov <aziz16110904@gmail.com>
2025-04-27 15:10:19 +0500

    Added Register part controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        return $this->respondWithToken(Auth::attempt(['email' => $user->email, 'password' => $request->password]));
+    }
     public function login(Request $request)
     {
         $credentials = $request->only(['email', 'password']);
